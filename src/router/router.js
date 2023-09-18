7c414ca69ba667812c2726e80205725eb4451781
Tanvir Alam <ta89725@gmail.com>
2023-09-18 10:49:50 +0600

    naviber part done

--- src/router/router.js
+++ src/router/router.js
@@ -1,3 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main/Main";
+import Home from "../pages/Home/Home";
+import NotFound from "../pages/NotFound/NotFound";
+import About from "../pages/About/About";
+import Appointment from "../pages/Appointment/Appointment";
+import Reviews from "../pages/Reviews/Reviews";
+import ContactUs from "../pages/ContactUs/ContactUs";
+import Login from "../pages/Login/Login";
 
-export const router = createBrowserRouter([])
+export const router = createBrowserRouter([
+    {
+        path: "/", element: <Main></Main>, children: ([
+            { path: "/", element: <Home></Home> },
+            { path: "/about", element: <About></About> },
+            { path: "/appointment", element: <Appointment></Appointment> },
+            { path: "/reviews", element: <Reviews></Reviews> },
+            { path: "/contract", element: <ContactUs></ContactUs> },
+            { path: "/login", element: <Login></Login> },
+            { path: "*", element: <NotFound></NotFound> }
+        ])
+    }
+])
